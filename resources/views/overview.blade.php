80cdefe5f849aae54f75fa3a8b3c4aa1a9be0d75
jerowm098 <jjjjema098@gmail.com>
2026-09-24 21:56:29 +0800

    feat: update overview greeting to include user role and name

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <h1 class="page-title">Overview</h1>
-    <p class="page-subtitle">Welcome back, {{ Auth::user()->name }}. Here is an overview of your inventory system.</p>
+    <p class="page-subtitle">Hello {{ ucfirst(Auth::user()->role) }} ({{ Auth::user()->name }}), here is an overview of your inventory system.</p>
 
     <!-- STATS GRID -->
     <div class="stats-grid">
